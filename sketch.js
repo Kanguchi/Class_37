d089889845affd27d43d942abca7431c59c8197a
KanoKit <kano.kitaguchi@gmail.com>
2020-10-11 17:47:03 -0400

    added sprites for cars and track, added gameEnd state

--- sketch.js
+++ sketch.js
@@ -9,6 +9,16 @@ var database;
 var form, player, game;
 
 var cars, car1, car2, car3, car4;
+var car1Img, car2Img, car3Img, car4Img, groundImg, trackImg;
+
+function preload(){
+  car1Img = loadImage("images/car1.png");
+  car2Img = loadImage("images/car2.png");
+  car3Img = loadImage("images/car3.png");
+  car4Img = loadImage("images/car4.png");
+  groundImg = loadImage("images/ground.png");
+  track = loadImage("images/track.jpg");
+}
 
 
 function setup(){
@@ -21,11 +31,15 @@ function setup(){
 
 
 function draw(){
+  background
   if(playerCount === 4){
     game.update(1);
   }
   if(gameState === 1){
     clear();
     game.play();
+  }
+  if(gameState === 2){
+    game.end();
   }
 }
